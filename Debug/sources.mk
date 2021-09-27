e6c2d3d1e05165fb2b74bbf82771cf629f8eecaf
SloopySky <61380776+SloopySky@users.noreply.github.com>
2021-09-27 20:37:35 +0200

    Add files via upload

---
+++ Debug/sources.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+EXECUTABLES := 
+OBJS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+FreeRTOS/Source/CMSIS_RTOS \
+FreeRTOS/Source \
+FreeRTOS/Source/portable/GCC/ARM_CM3 \
+FreeRTOS/Source/portable/MemMang \
+HAL_Driver/Src \
+Source/ApplicationLayer \
+Source/HardwareLayer \
+Source \
+startup \
+
